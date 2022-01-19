4f295be3b85acbb5c9e36c5b859bc6fe4f3ba9e6
hmtmcse <hmtm.cse@gmail.com>
2022-01-19 11:11:50 +0600

    added dependency

--- tools/dev_manager.py
+++ tools/dev_manager.py
@@ -78,7 +78,12 @@ def clone_pull_setup(projects: dict):
 source_projects = {
     "dir": LIBRARY_DEPENDENCY,
     "repositories": {
-        "xxxxxx": "https://github.com/problemfighter/xxxxxxx.git",
+        "pf-flask-rest-com": "https://github.com/problemfighter/pf-flask-rest-com.git",
+        "pf-flask-rest": "https://github.com/problemfighter/pf-flask-rest.git",
+        "pf-flask-db": "https://github.com/problemfighter/pf-flask-db.git",
+        "pf-flask-auth": "https://github.com/problemfighter/pf-flask-auth.git",
+        "pf-py-common": "https://github.com/problemfighter/pf-py-common.git",
+        "pf-flask-swagger": "https://github.com/problemfighter/pf-flask-swagger.git",
     }
 }
 
