7360800660baa0722d19d6db57ca9ee2336a1d46
hmtmcse <hmtm.cse@gmail.com>
2022-01-21 21:59:23 +0600

    added pweb cli

--- pf_flask_web/bismillah.py
+++ pf_flask_web/bismillah.py
@@ -1,25 +1,50 @@
 import os
-
+import typing as t
 from flask_cors import CORS
-from flask import Flask
+from pf_flask_web.system12.pweb import PWeb
 from pf_flask_web.system12.pweb_app_config import PWebAppConfig
 from pf_flask_web.system12.pweb_registry import PWebRegistry
 from pf_py_ymlenv import yaml_env
 
 
 class Bismillah(object):
-    _pweb_app: Flask
+    _pweb_app: PWeb
     _config: PWebAppConfig = None
 
-    def __init__(self, project_root_path, config=PWebAppConfig(), name="PWebApp"):
-        self._pweb_app = Flask(name)
+    def __init__(
+        self,
+        project_root_path,
+        config=PWebAppConfig(),
+        name: str = "PWebApp",
+        static_url_path: t.Optional[str] = None,
+        static_folder: t.Optional[t.Union[str, os.PathLike]] = "static",
+        static_host: t.Optional[str] = None,
+        host_matching: bool = False,
+        subdomain_matching: bool = False,
+        template_folder: t.Optional[str] = "templates",
+        instance_path: t.Optional[str] = None,
+        instance_relative_config: bool = False,
+        root_path: t.Optional[str] = None,
+    ):
+        self._pweb_app = PWeb(
+            name,
+            static_url_path=static_url_path,
+            static_folder=static_folder,
+            static_host=static_host,
+            host_matching=host_matching,
+            subdomain_matching=subdomain_matching,
+            template_folder=template_folder,
+            instance_path=instance_path,
+            instance_relative_config=instance_relative_config,
+            root_path=root_path,
+        )
         self._config = config
         self._process_project_root_path(root_path=project_root_path)
         self._init_config()
         self._init_cors()
 
     def run(self):
-        self._pweb_app.run(host=self._config.HOST, port=self._config.PORT)
+        self._pweb_app.run(host=self._config.HOST, port=self._config.PORT, load_dotenv=False)
 
     def get_app(self):
         return self._pweb_app
--- pweb_manage.py
+++ pweb_manage.py
@@ -5,12 +5,15 @@ from pf_flask_web import Bismillah
 
 def pweb_factory():
     pweb_app = Bismillah(__file__)
-    return pweb_app.get_app()
+    return pweb_app
 
 
-@click.group(cls=FlaskGroup, create_app=pweb_factory)
+@click.group(cls=FlaskGroup, create_app=pweb_factory().get_app)
 def cli():
     print("Welcome to PWeb CLI")
+
+
+wsgi = pweb_factory().get_app()
 
 
 if __name__ == '__main__':
