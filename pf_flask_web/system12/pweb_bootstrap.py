a251bb95b67dcfa2c658f694763b0ab142a8c4fa
hmtmcse <hmtm.cse@gmail.com>
2022-01-22 00:40:10 +0600

    added all engine

--- pf_flask_web/system12/pweb_bootstrap.py
+++ pf_flask_web/system12/pweb_bootstrap.py
@@ -1,7 +1,10 @@
 from flask import render_template
 
+from pf_flask_rest.pf_flask_rest import pf_flask_rest
+from pf_flask_swagger.flask.pf_flask_swagger import PFFlaskSwagger
 from pf_flask_web.system12.pweb import PWeb
 from pf_flask_web.system12.pweb_app_config import PWebAppConfig
+from pf_flask_web.system12.pweb_db import pweb_db
 from pf_flask_web.system12.pweb_flask_util import PWebFlaskUtil
 
 
@@ -13,15 +16,17 @@ class PwebBootstrap:
         self._config = config
         self._pweb_app = pweb_app
         self._init_default_page()
+        self._init_db()
+        self._init_swagger_doc()
 
     def _init_swagger_doc(self):
-        pass
+        PFFlaskSwagger(self._pweb_app)
 
     def _init_db(self):
-        pass
+        pweb_db.init_app(self._pweb_app)
 
     def _init_rest_engine(self):
-        pass
+        pf_flask_rest.init_app(self._pweb_app)
 
     def _default_html(self):
         return render_template(self._config.DEFAULT_HTML)
---
+++ pf_flask_web/system12/pweb_db.py
@@ -0,0 +1,3 @@
+from pf_flask_db.pf_app_database import PFAppDatabase
+
+pweb_db = PFAppDatabase()
